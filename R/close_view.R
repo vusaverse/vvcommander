22cff1ee70070fbedc482269d0704c887004fcae
Tomer Iwan <t.iwan@vu.nl>
2023-10-03 12:29:02 +0200

    update: roxygen comments

--- R/close_view.R
+++ R/close_view.R
@@ -1,8 +1,12 @@
-#' close_view.
+#' Close All View Tabs in RStudio
 #'
-#' Close all view tabs.
-#' Note: this also closes unsaved files.
-#' You will be prompted to save before these files are closed.
+#' This function closes all view tabs in RStudio. It starts from the current document and moves to the next tab until it reaches the starting document again.
+#'
+#' Note that this function will prompt you to save any unsaved files. If you choose not to save, these files will be closed without saving.
+#'
+#' @note This function requires the `rstudioapi` package, which is not automatically included in R. You need to install it manually if you haven't done so.
+#'
+#' @return None
 #'
 #' @export
 close_view <- function() {
